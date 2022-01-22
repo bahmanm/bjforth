546a99d760bd09c588258aec619f7cc37b83419c
Bahman Movaqar <Bahman@BahmanM.com>
2022-01-22 13:02:15 -0800

    use FailableCallable to reduce the need for copy-paste
    no changes to the interface though

--- bjforth/src/test-lib/java/bjforth/machine/MachineStateInspectionUtils.java
+++ bjforth/src/test-lib/java/bjforth/machine/MachineStateInspectionUtils.java
@@ -7,64 +7,64 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.commons.lang3.function.FailableCallable;
+
 public class MachineStateInspectionUtils {
 
-  @SuppressWarnings("unchecked")
-  public static Set<Integer> memoryAddresses(MachineState ms) {
+  private static <R> R inspect(FailableCallable<R,IllegalAccessException> inspector) {
     try {
-      var cells = (Map<Integer,?>) readDeclaredField(ms.getMemory(), "cells", true);
-      return cells.keySet();
+      return inspector.call();
     } catch (IllegalAccessException e) {
       throw new RuntimeException(e);
     }
+  }
+
+  @SuppressWarnings("unchecked")
+  public static Set<Integer> memoryAddresses(MachineState ms) {
+    return inspect(() -> {
+        var cells = (Map<Integer,?>) readDeclaredField(ms.getMemory(), "cells", true);
+        return cells.keySet();
+      });
   }
 
   @SuppressWarnings("unchecked")
   public static Set<String> dictionaryKeys(MachineState ms) {
-    try {
-      var items = (Map<String,?>) readDeclaredField(ms.getDictionary(), "items", true);
-      return items.keySet();
-    } catch (IllegalAccessException e) {
-      throw new RuntimeException(e);
-    }
+    return inspect(() -> {
+        var items = (Map<String, ?>) readDeclaredField(ms.getDictionary(), "items", true);
+        return items.keySet();
+
+      });
   }
 
   public static Integer returnStackSize(MachineState ms) {
-    try {
-      var data = (Deque<?>) readDeclaredField(ms.getReturnStack(), "data", true);
-      return data.size();
-    } catch (IllegalAccessException e) {
-      throw new RuntimeException(e);
-    }
+    return inspect(() -> {
+        var data = (Deque<?>) readDeclaredField(ms.getReturnStack(), "data", true);
+        return data.size();
+      });
   }
 
   @SuppressWarnings("unchecked")
   public static Iterator<Integer> returnStackDescendingIterator(MachineState ms) {
-    try {
-      var data = (Deque<Integer>) readDeclaredField(ms.getReturnStack(), "data", true);
-      return data.descendingIterator();
-    } catch (IllegalAccessException e) {
-      throw new RuntimeException(e);
-    }
+    return inspect(() -> {
+        var data = (Deque<Integer>) readDeclaredField(ms.getReturnStack(), "data", true);
+        return data.descendingIterator();
+      });
   }
 
   public static Integer parameterStackSize(MachineState ms) {
-    try {
-      var data = (Deque<?>) readDeclaredField(ms.getParameterStack(), "data", true);
-      return data.size();
-    } catch (IllegalAccessException e) {
-      throw new RuntimeException(e);
-    }
+    return inspect(() -> {
+        var data = (Deque<?>) readDeclaredField(ms.getParameterStack(), "data", true);
+        return data.size();
+      });
   }
 
   @SuppressWarnings("unchecked")
   public static Iterator<Object> parameterStackDescendingIterator(MachineState ms) {
-    try {
-      var data = (Deque<Object>) readDeclaredField(ms.getParameterStack(), "data", true);
-      return data.descendingIterator();
-    } catch (IllegalAccessException e) {
-      throw new RuntimeException(e);
-    }
+    return inspect(() -> {
+        var data = (Deque<Object>) readDeclaredField(ms.getParameterStack(), "data", true);
+        return data.descendingIterator();
+
+      });
   }
 
   public static Integer instructionPointer(MachineState ms) {
