e4be2f0e76d4994172e67884117db94e20a89794
Bahman Movaqar <bahman@bahmanm.com>
2022-04-19 22:40:34 -0700

    simplify ?DUP to use NumberOperations

--- bjforth/src/main/java/bjforth/primitives/QDUP.java
+++ bjforth/src/main/java/bjforth/primitives/QDUP.java
@@ -20,8 +20,7 @@ package bjforth.primitives;
 
 import bjforth.machine.Machine;
 import bjforth.machine.MachineException;
-import java.math.BigDecimal;
-import java.math.BigInteger;
+import bjforth.primitives.lib.NumberFunctions;
 import java.util.NoSuchElementException;
 
 public class QDUP implements MachinePrimitiveWithNext {
@@ -30,34 +29,12 @@ public class QDUP implements MachinePrimitiveWithNext {
   public void executeWithNext(Machine machine) {
     try {
       var element = machine.popFromParameterStack();
-      if (!isZero(element)) {
+      if (!NumberFunctions.isZero.apply(element)) {
         machine.pushToParameterStack(element);
       }
       machine.pushToParameterStack(element);
     } catch (NoSuchElementException ex) {
       throw new MachineException("ParameterStack error.");
-    }
-  }
-
-  private boolean isZero(Object obj) {
-    if (obj instanceof BigDecimal bd) {
-      return BigDecimal.ZERO.equals(bd);
-    } else if (obj instanceof BigInteger bi) {
-      return BigInteger.ZERO.equals(bi);
-    } else if (obj instanceof Byte b) {
-      return b == 0;
-    } else if (obj instanceof Double d) {
-      return d == 0d;
-    } else if (obj instanceof Float f) {
-      return f == 0f;
-    } else if (obj instanceof Integer i) {
-      return i == 0;
-    } else if (obj instanceof Long l) {
-      return l == 0l;
-    } else if (obj instanceof Short s) {
-      return s == 0;
-    } else {
-      throw new MachineException("ParameterStack top not a number.");
     }
   }
 }
