411da3fa89ab38dcd6eb78b24c7466c9a311a262
Bahman Movaqar <bahman@bahmanm.com>
2022-07-05 06:42:41 -0700

    impl LIT primitive (#13)

---
+++ bjforth/src/test/java/bjforth/primitives/LITTest.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2022 Bahman Movaqar
+ *
+ * This file is part of BJForth.
+ *
+ * BJForth is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BJForth is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BJForth. If not, see <https://www.gnu.org/licenses/>.
+ */
+package bjforth.primitives;
+
+import static bjforth.machine.InstructionPointerBuilder.anInstructionPointer;
+import static bjforth.machine.MachineAssertions.*;
+import static bjforth.machine.MachineStateBuilder.aMachineState;
+import static bjforth.machine.MemoryBuilder.aMemory;
+import static bjforth.machine.NextInstructionPointerBuilder.aNextInstructionPointer;
+import static bjforth.machine.ParameterStackBuilder.aParameterStack;
+import static bjforth.utils.RandomUtils.nextInt;
+import static org.assertj.core.api.Assertions.*;
+
+import bjforth.machine.Machine;
+import bjforth.utils.RandomUtils;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.stream.Stream;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+class LITTest {
+
+  @DisplayName("should place the literal on stack and increase `nextInstructionPointer` by 2")
+  @ParameterizedTest(name = "{displayName} literal(type={1}, value={0})")
+  @ArgumentsSource(LiteralProvider.class)
+  void worksOk(Object literal, String literalClassName) {
+    // GIVEN
+    var lit = new LIT();
+    var litAddr = nextInt();
+    var ip = anInstructionPointer().with(litAddr).build();
+    var nip = aNextInstructionPointer().with(ip).plus(1).build();
+    var state1 =
+        aMachineState()
+            .withInstrcutionPointer(ip)
+            .withNextInstructionPointer(nip)
+            .withMemory(aMemory().with(litAddr, lit).with(nip, literal).build())
+            .withParameterStack(aParameterStack().build())
+            .build();
+    var state2 = aMachineState().copyFrom(state1).build();
+    var machine = new Machine(state2);
+
+    // WHEN
+    machine.step();
+
+    // THEN
+    assertThat(state2)
+        .hasInstructionPointerEqualTo(anInstructionPointer().with(state1).plus(2).build())
+        .hasNextInstructionPointerEqualTo(aNextInstructionPointer().with(state1).plus(2).build())
+        .hasDictionaryEqualTo(state1)
+        .hasMemoryEqualTo(state1)
+        .hasParameterStackEqualTo(aParameterStack().with(literal).build())
+        .hasReturnStackEqualTo(state1);
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  static class LiteralProvider implements ArgumentsProvider {
+
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
+      return Stream.of(
+          Arguments.of(RandomUtils.nextBigDecimal(), BigDecimal.class.getCanonicalName()),
+          Arguments.of(RandomUtils.nextBigInteger(), BigInteger.class.getCanonicalName()),
+          Arguments.of(RandomUtils.nextLong(), Long.class.getCanonicalName()),
+          Arguments.of(RandomStringUtils.random(20), String.class.getCanonicalName()),
+          Arguments.of(new Object(), Object.class.getCanonicalName()));
+    }
+  }
+}
