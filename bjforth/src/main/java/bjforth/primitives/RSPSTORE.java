d772404e4d2fb28cd0e93c00ca622d87e69e377b
Bahman Movaqar <Bahman@BahmanM.com>
2023-05-26 13:53:19 -0700

    impl RSPSTORE primitive

---
+++ bjforth/src/main/java/bjforth/primitives/RSPSTORE.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2023 Bahman Movaqar
+ *
+ * This file is part of BJForth.
+ *
+ * BJForth is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BJForth is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BJForth. If not, see <https://www.gnu.org/licenses/>.
+ */
+package bjforth.primitives;
+
+import bjforth.machine.Machine;
+import bjforth.machine.MachineException;
+import java.util.NoSuchElementException;
+
+public class RSPSTORE implements MachinePrimitiveWithNext {
+
+  @Override
+  public void executeWithNext(Machine machine) {
+    try {
+      var pointerObject = machine.popFromParameterStack();
+      if (pointerObject instanceof Integer pointer) {
+        machine.setReturnStackPointer(pointer);
+      } else {
+        throw new MachineException("Invalid stack pointer");
+      }
+    } catch (NoSuchElementException ex) {
+      throw new MachineException("ParameterStack error.");
+    }
+  }
+}
