03b50d7661d244e46855a872b7456974504a3f14
Bahman Movaqar <bahman@bahmanm.com>
2024-11-25 20:20:20 -0800

    Rename BJForth to bjForth and hence the (C) header changes. (#93)

--- bjforth/src/test/java/bjforth/primitives/HERETest.java
+++ bjforth/src/test/java/bjforth/primitives/HERETest.java
@@ -1,20 +1,20 @@
 /*
  * Copyright 2024 Bahman Movaqar
  *
- * This file is part of BJForth.
+ * This file is part of bjForth.
  *
- * BJForth is free software: you can redistribute it and/or modify it
+ * bjForth is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * BJForth is distributed in the hope that it will be useful, but WITHOUT
+ * bjForth is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  * for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with BJForth. If not, see <https://www.gnu.org/licenses/>.
+ * along with bjForth. If not, see <https://www.gnu.org/licenses/>.
  */
 package bjforth.primitives;
 
