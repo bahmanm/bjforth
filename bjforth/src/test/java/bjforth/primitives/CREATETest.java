7ca60e9f31db3f44d18216e6b4b7382a834a1069
Bahman Movaqar <bahman@bahmanm.com>
2024-12-01 20:53:19 -0800

    Refactor tests (#113)

--- bjforth/src/test/java/bjforth/primitives/CREATETest.java
+++ bjforth/src/test/java/bjforth/primitives/CREATETest.java
@@ -18,6 +18,7 @@
  */
 package bjforth.primitives;
 
+import static bjforth.machine.BootstrapUtils.getPrimitiveAddress;
 import static bjforth.machine.DictionaryBuilder.aDictionary;
 import static bjforth.machine.InstructionPointerBuilder.anInstructionPointer;
 import static bjforth.machine.MachineAssertions.assertThat;
@@ -42,66 +43,64 @@ class CREATETest {
   @DisplayName("Create the dictionary item and the header.")
   public void worksOk() {
     // GIVEN
-    var create = PrimitiveFactory.CREATE();
-    var createAddr = nextInt();
-    var ip = anInstructionPointer().with(createAddr).build();
-    var nip = aNextInstructionPointer().with(ip).plus(1).build();
+    var CREATEaddr = getPrimitiveAddress("CREATE");
     var nameAddr = nextInt();
-    var name = RandomStringUtils.secure().next(10);
+    var name = RandomStringUtils.insecure().next(10);
     var nameLength = name.length();
-    var hereValue = nextInt();
-    var state1 =
+    var actualState =
         aMachineState()
-            .withInstrcutionPointer(ip)
-            .withNextInstructionPointer(nip)
-            .withMemory(aMemory().with(createAddr, create).with(nameAddr, name).build())
+            .withInstrcutionPointer(CREATEaddr)
+            .withNextInstructionPointer(CREATEaddr + 1)
+            .withMemory(aMemory().with(nameAddr, name).build())
             .withParameterStack(aParameterStack().with(nameAddr, nameLength).build())
             .withVariable(Variables.get("LATEST"), 0)
-            .withVariable(Variables.get("HERE"), hereValue)
             .build();
-    var state2 = aMachineState().copyFrom(state1).build();
-    var machine = aMachine().withState(state2).build();
+    var machine = aMachine().withState(actualState).build();
+    var referenceState = aMachineState().copyFrom(actualState).build();
+    var HEREaddr = Variables.get("HERE").getAddress();
+    var HEREvalue = (Integer) machine.getMemoryAt(HEREaddr);
 
     // WHEN
     machine.step();
 
     // THEN
-    assertThat(state2)
-        .hasInstructionPointerEqualTo(anInstructionPointer().with(state1).plus(1).build())
-        .hasNextInstructionPointerEqualTo(aNextInstructionPointer().with(state1).plus(1).build())
+    assertThat(actualState)
+        .hasInstructionPointerEqualTo(anInstructionPointer().with(referenceState).plus(1).build())
+        .hasNextInstructionPointerEqualTo(
+            aNextInstructionPointer().with(referenceState).plus(1).build())
         .hasDictionaryEqualTo(
-            aDictionary().with(name, new DictionaryItem(name, hereValue, false, true)).build())
+            aDictionary()
+                .with(referenceState)
+                .with(name, new DictionaryItem(name, HEREvalue, false, true))
+                .build())
         .hasMemoryEqualTo(
             aMemory()
-                .with(state1)
-                .with(Variables.get("HERE").getAddress(), hereValue + 1)
-                .with(Variables.get("LATEST").getAddress(), hereValue)
-                .with(hereValue, name)
+                .with(actualState)
+                .with(HEREaddr, HEREvalue + 1)
+                .with(Variables.get("LATEST").getAddress(), HEREvalue)
+                .with(HEREvalue, name)
                 .build())
         .hasParameterStackEqualTo(aParameterStack().build())
-        .hasReturnStackEqualTo(state1);
+        .hasReturnStackEqualTo(actualState);
   }
 
   @Test
   @DisplayName("Should throw if ParameterStack is already empty.")
   public void throwIfEmpty() {
     // GIVEN
-    var create = PrimitiveFactory.CREATE();
-    var createAddr = nextInt();
-    var ip = anInstructionPointer().with(createAddr).build();
-    var nip = aNextInstructionPointer().with(ip).plus(1).build();
-    var state1 =
+    var CREATEaddr = getPrimitiveAddress("CREATE");
+    var actualState =
         aMachineState()
-            .withInstrcutionPointer(ip)
-            .withNextInstructionPointer(nip)
-            .withMemory(aMemory().with(createAddr, create).build())
+            .withInstrcutionPointer(CREATEaddr)
+            .withNextInstructionPointer(CREATEaddr + 1)
+            .withMemory(aMemory().build())
             .withParameterStack(aParameterStack().build())
             .build();
-    var state2 = aMachineState().copyFrom(state1).build();
-    var machine = aMachine().withState(state2).build();
+    var machine = aMachine().withState(actualState).build();
+    var referenceState = aMachineState().copyFrom(actualState).build();
 
     // EXPECT
     assertThrows(MachineException.class, machine::step);
-    assertThat(state2).isEqualTo(aMachineState().copyFrom(state1).build());
+    assertThat(actualState).isEqualTo(aMachineState().copyFrom(referenceState).build());
   }
 }
