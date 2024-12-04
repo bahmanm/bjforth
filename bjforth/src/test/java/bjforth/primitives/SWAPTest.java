3229c79f30d15e87a5e3cb8de89a00b4c4c297b4
Bahman Movaqar <bahman@bahmanm.com>
2024-12-03 19:39:38 -0800

    Refactor tests (#158)

--- bjforth/src/test/java/bjforth/primitives/SWAPTest.java
+++ bjforth/src/test/java/bjforth/primitives/SWAPTest.java
@@ -18,11 +18,11 @@
  */
 package bjforth.primitives;
 
+import static bjforth.machine.BootstrapUtils.getPrimitiveAddress;
 import static bjforth.machine.InstructionPointerBuilder.anInstructionPointer;
 import static bjforth.machine.MachineAssertions.*;
 import static bjforth.machine.MachineBuilder.aMachine;
 import static bjforth.machine.MachineStateBuilder.aMachineState;
-import static bjforth.machine.MemoryBuilder.aMemory;
 import static bjforth.machine.NextInstructionPointerBuilder.aNextInstructionPointer;
 import static bjforth.machine.ParameterStackBuilder.aParameterStack;
 import static org.apache.commons.lang3.RandomUtils.nextInt;
@@ -39,102 +39,72 @@ class SWAPTest {
   @DisplayName("swaps the two top elements of the ParameterStack.")
   public void worksOk() {
     // GIVEN
-    var swap = PrimitiveFactory.SWAP();
-    var swapAddr = nextInt();
-    var ip = anInstructionPointer().with(swapAddr).build();
-    var nip = aNextInstructionPointer().with(ip).plus(1).build();
+    var SWAPaddr = getPrimitiveAddress("SWAP");
     var parameter1 = nextInt();
     var parameter2 = nextInt();
-    var state1 =
+    var actualState =
         aMachineState()
-            .withInstrcutionPointer(ip)
-            .withNextInstructionPointer(nip)
-            .withMemory(aMemory().with(swapAddr, swap).build())
+            .withInstrcutionPointer(SWAPaddr)
+            .withNextInstructionPointer(SWAPaddr + 1)
             .withParameterStack(aParameterStack().with(parameter1, parameter2).build())
             .build();
-    var state2 = aMachineState().copyFrom(state1).build();
-    var machine = aMachine().withState(state2).build();
+    var machine = aMachine().withState(actualState).build();
+    var referenceState = aMachineState().copyFrom(actualState).build();
 
     // WHEN
     machine.step();
 
     // THEN
-    assertThat(state2)
-        .hasInstructionPointerEqualTo(anInstructionPointer().with(state1).plus(1).build())
-        .hasNextInstructionPointerEqualTo(aNextInstructionPointer().with(state1).plus(1).build())
-        .hasDictionaryEqualTo(state1)
-        .hasMemoryEqualTo(state1)
+    assertThat(actualState)
+        .hasInstructionPointerEqualTo(anInstructionPointer().with(referenceState).plus(1).build())
+        .hasNextInstructionPointerEqualTo(
+            aNextInstructionPointer().with(referenceState).plus(1).build())
+        .hasDictionaryEqualTo(referenceState)
+        .hasMemoryEqualTo(referenceState)
         .hasParameterStackEqualTo(aParameterStack().with(parameter2, parameter1).build())
-        .hasReturnStackEqualTo(state1);
+        .hasReturnStackEqualTo(referenceState);
   }
 
   @Test
   @DisplayName("should throw if ParameterStack is already empty.")
   public void throwIfEmpty() {
     // GIVEN
-    var swap = PrimitiveFactory.SWAP();
-    var swapAddr = nextInt();
-    var ip = anInstructionPointer().with(swapAddr).build();
-    var nip = aNextInstructionPointer().with(ip).plus(1).build();
-    var state1 =
+    var SWAPaddr = getPrimitiveAddress("SWAP");
+    var actualState =
         aMachineState()
-            .withInstrcutionPointer(ip)
-            .withNextInstructionPointer(nip)
-            .withMemory(aMemory().with(swapAddr, swap).build())
+            .withInstrcutionPointer(SWAPaddr)
+            .withNextInstructionPointer(SWAPaddr + 1)
             .withParameterStack(aParameterStack().build())
             .build();
-    var state2 = aMachineState().copyFrom(state1).build();
-    var machine = aMachine().withState(state2).build();
+    var machine = aMachine().withState(actualState).build();
+    var referenceState = aMachineState().copyFrom(actualState).build();
 
     // EXPECT
     assertThrows(MachineException.class, machine::step);
-    assertThat(state2).isEqualTo(aMachineState().copyFrom(state1).build());
+    assertThat(actualState).isEqualTo(aMachineState().copyFrom(referenceState).build());
   }
 
   @Test
   @DisplayName("should throw if ParameterStack has only 1 element.")
   public void throwIfOneElement() {
     // GIVEN
-    var swap = PrimitiveFactory.SWAP();
-    var swapAddr = nextInt();
-    var ip = anInstructionPointer().with(swapAddr).build();
-    var nip = aNextInstructionPointer().with(ip).plus(1).build();
-    var state1 =
+    var SWAPaddr = getPrimitiveAddress("SWAP");
+    var actualState =
         aMachineState()
-            .withInstrcutionPointer(ip)
-            .withNextInstructionPointer(nip)
-            .withMemory(aMemory().with(swapAddr, swap).build())
+            .withInstrcutionPointer(SWAPaddr)
+            .withNextInstructionPointer(SWAPaddr + 1)
             .withParameterStack(aParameterStack().with(nextInt()).build())
             .build();
-    var state2 = aMachineState().copyFrom(state1).build();
-    var machine = aMachine().withState(state2).build();
+    var machine = aMachine().withState(actualState).build();
+    var referenceState = aMachineState().copyFrom(actualState).build();
 
     // EXPECT
     assertThrows(MachineException.class, machine::step);
-  }
-
-  @Test
-  @DisplayName("should drop the top of ParameterStack if it has only 1 element.")
-  public void dropIfOneElement() {
-    // GIVEN
-    var swap = PrimitiveFactory.SWAP();
-    var swapAddr = nextInt();
-    var ip = anInstructionPointer().with(swapAddr).build();
-    var nip = aNextInstructionPointer().with(ip).plus(1).build();
-    var state1 =
-        aMachineState()
-            .withInstrcutionPointer(ip)
-            .withNextInstructionPointer(nip)
-            .withMemory(aMemory().with(swapAddr, swap).build())
-            .withParameterStack(aParameterStack().with(nextInt()).build())
-            .build();
-    var state2 = aMachineState().copyFrom(state1).build();
-    var machine = aMachine().withState(state2).build();
-
-    // EXPECT
-    assertThrows(MachineException.class, machine::step);
-    assertThat(state2)
+    assertThat(actualState)
         .isEqualTo(
-            aMachineState().copyFrom(state1).withParameterStack(aParameterStack().build()).build());
+            aMachineState()
+                .copyFrom(referenceState)
+                .withParameterStack(aParameterStack().build())
+                .build());
   }
 }
