0ad9845f1c76521ce0f54a4dbb7bc54faa16490b
Bahman Movaqar <bahman@bahmanm.com>
2024-12-10 18:04:30 -0800

    Implement the SEMICOLON primitive

---
+++ bjforth/src/main/java/bjforth/primitives/SEMICOLON.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2024 Bahman Movaqar
+ *
+ * This file is part of bjForth.
+ *
+ * bjForth is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * bjForth is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with bjForth. If not, see <https://www.gnu.org/licenses/>.
+ */
+package bjforth.primitives;
+
+import static bjforth.primitives.PrimitiveFactory.*;
+
+import bjforth.machine.Machine;
+import bjforth.variables.Variables;
+
+public class SEMICOLON implements Primitive {
+  @Override
+  public void execute(Machine machine) {
+    var HEREvalue = (Integer) machine.getMemoryAt(Variables.get("HERE").getAddress());
+    machine.setMemoryAt(HEREvalue, machine.getDictionaryItem("EXIT").get().getAddress());
+    var LATESTvalue = (Integer) machine.getMemoryAt(Variables.get("LATEST").getAddress());
+    machine.pushToParameterStack(LATESTvalue);
+    HIDDEN().execute(machine);
+    LBRAC().execute(machine);
+    EXIT().execute(machine);
+  }
+
+  @Override
+  public Boolean isImmediate() {
+    return true;
+  }
+
+  @Override
+  public String getName() {
+    return ";";
+  }
+
+  @Override
+  public Boolean isBypassNextInstructionPointer() {
+    return true;
+  }
+}
