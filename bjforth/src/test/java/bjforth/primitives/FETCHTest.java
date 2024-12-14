361ecc47ec6d2c1adb9458df6f4bb1842f68bd05
Bahman Movaqar <bahman@bahmanm.com>
2024-12-13 19:52:23 -0800

    Fix tests RE NIP

--- bjforth/src/test/java/bjforth/primitives/FETCHTest.java
+++ bjforth/src/test/java/bjforth/primitives/FETCHTest.java
@@ -18,12 +18,10 @@
  */
 package bjforth.primitives;
 
-import static bjforth.machine.InstructionPointerBuilder.anInstructionPointer;
 import static bjforth.machine.MachineAssertions.*;
 import static bjforth.machine.MachineBuilder.aMachine;
 import static bjforth.machine.MachineStateBuilder.aMachineState;
 import static bjforth.machine.MemoryBuilder.aMemory;
-import static bjforth.machine.NextInstructionPointerBuilder.aNextInstructionPointer;
 import static bjforth.machine.ParameterStackBuilder.aParameterStack;
 import static org.assertj.core.api.Assertions.*;
 
@@ -46,7 +44,6 @@ class FETCHTest {
     var actualState =
         aMachineState()
             .withInstrcutionPointer(FETCHaddr)
-            .withNextInstructionPointer(FETCHaddr + 1)
             .withMemory(aMemory().with(addrToFetch, objectToFetch).build())
             .withParameterStack(aParameterStack().with(addrToFetch).build())
             .build();
@@ -57,14 +54,7 @@ class FETCHTest {
     machine.step();
 
     // THEN
-    assertThat(actualState)
-        .hasInstructionPointerEqualTo(anInstructionPointer().with(referenceState).plus(1).build())
-        .hasNextInstructionPointerEqualTo(
-            aNextInstructionPointer().with(referenceState).plus(1).build())
-        .hasDictionaryEqualTo(referenceState)
-        .hasMemoryEqualTo(aMemory().with(referenceState).build())
-        .hasParameterStackEqualTo(aParameterStack().with(objectToFetch).build())
-        .hasReturnStackEqualTo(referenceState);
+    assertThat(actualState).hasParameterStackEqualTo(aParameterStack().with(objectToFetch).build());
   }
 
   @DisplayName("should throw if parameter stack top is not a number.")
