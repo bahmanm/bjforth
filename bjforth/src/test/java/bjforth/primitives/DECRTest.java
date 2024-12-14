6454f881433e4272c3da839859eddc00401c2eff
Bahman Movaqar <bahman@bahmanm.com>
2024-12-13 17:16:48 -0800

    Fix tests RE NIP

--- bjforth/src/test/java/bjforth/primitives/DECRTest.java
+++ bjforth/src/test/java/bjforth/primitives/DECRTest.java
@@ -18,12 +18,10 @@
  */
 package bjforth.primitives;
 
-import static bjforth.machine.InstructionPointerBuilder.anInstructionPointer;
 import static bjforth.machine.MachineAssertions.*;
 import static bjforth.machine.MachineBuilder.aMachine;
 import static bjforth.machine.MachineStateBuilder.aMachineState;
 import static bjforth.machine.MemoryBuilder.aMemory;
-import static bjforth.machine.NextInstructionPointerBuilder.aNextInstructionPointer;
 import static bjforth.machine.ParameterStackBuilder.aParameterStack;
 import static org.assertj.core.api.Assertions.*;
 
@@ -66,13 +64,7 @@ class DECRTest {
 
     // THEN
     assertThat(actualState)
-        .hasInstructionPointerEqualTo(anInstructionPointer().with(referenceState).plus(1).build())
-        .hasNextInstructionPointerEqualTo(
-            aNextInstructionPointer().with(referenceState).plus(1).build())
-        .hasDictionaryEqualTo(referenceState)
-        .hasMemoryEqualTo(referenceState)
-        .hasParameterStackEqualTo(aParameterStack().with(expectedResult).build())
-        .hasReturnStackEqualTo(referenceState);
+        .hasParameterStackEqualTo(aParameterStack().with(expectedResult).build());
   }
 
   @DisplayName("should throw if top of parameter stack is not a number.")
