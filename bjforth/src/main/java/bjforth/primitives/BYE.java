8c22e8ddd00a1de82ea6b6b7747471349b0bfafc
Bahman Movaqar <bahman@bahmanm.com>
2024-12-16 20:10:56 -0800

    Implement the BYE primitive

---
+++ bjforth/src/main/java/bjforth/primitives/BYE.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2024 Bahman Movaqar
+ *
+ * This file is part of bjForth.
+ *
+ * bjForth is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * bjForth is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with bjForth. If not, see <https://www.gnu.org/licenses/>.
+ */
+package bjforth.primitives;
+
+import bjforth.machine.Machine;
+
+public class BYE implements Primitive {
+  @Override
+  public void execute(Machine machine) {
+    System.out.println("BYE");
+    System.out.flush();
+    System.exit(0);
+  }
+}
