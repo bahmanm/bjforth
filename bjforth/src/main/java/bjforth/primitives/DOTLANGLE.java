13b80be010a63e6603034663a179711231576d30
Bahman Movaqar <bahman@bahmanm.com>
2024-12-22 21:04:34 -0800

    Implement the DOTLANGLE primitive

---
+++ bjforth/src/main/java/bjforth/primitives/DOTLANGLE.java
@@ -0,0 +1,221 @@
+/*
+ * Copyright 2024 Bahman Movaqar
+ *
+ * This file is part of bjForth.
+ *
+ * bjForth is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * bjForth is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with bjForth. If not, see <https://www.gnu.org/licenses/>.
+ */
+package bjforth.primitives;
+
+import static bjforth.primitives.PrimitiveFactory.KEY;
+
+import bjforth.machine.Machine;
+import bjforth.machine.MachineException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class DOTLANGLE implements Primitive {
+
+  private HashMap<String, Class<?>> clazzCache = new HashMap<>();
+
+  private static enum State {
+    BEGIN,
+    IN_METHOD_NAME,
+    IN_PARAM_TYPE,
+    IN_MAYBE_VARARG,
+    END
+  }
+
+  static class MethodDescriptor {
+    String name = "";
+    List<Class<?>> parameterTypes = new ArrayList<>();
+    Boolean isVarargs = false;
+  }
+
+  @Override
+  public void execute(Machine machine) {
+    var name = new StringBuilder();
+    var state = State.BEGIN;
+    var result = new MethodDescriptor();
+    var parameterType = new StringBuilder();
+    while (!state.equals(State.END)) {
+      KEY().execute(machine);
+      var s = (String) machine.popFromParameterStack();
+      switch (state) {
+        case State.BEGIN:
+          if (!" ".equals(s) && !"\t".equals(s)) {
+            state = State.IN_METHOD_NAME;
+            name.append(s);
+          }
+          break;
+        case State.IN_METHOD_NAME:
+          if ("(".equals(s)) {
+            state = State.IN_PARAM_TYPE;
+          } else if (!" ".equals(s) && !"\t".equals(s)) {
+            name.append(s);
+            result.name = name.toString();
+          }
+          break;
+        case State.IN_PARAM_TYPE:
+          if ("(".equals(s)) {
+            state = State.END;
+          } else if (" ".equals(s) || "\t".equals(s) || "\n".equals(s)) {
+            // Ignore whitespace
+          } else if (",".equals(s)) {
+            result.parameterTypes.add(forName(parameterType.toString()));
+            parameterType = new StringBuilder();
+          } else if (".".equals(s)) {
+            state = State.IN_MAYBE_VARARG;
+          } else {
+            parameterType.append(s);
+          }
+          break;
+        case State.IN_MAYBE_VARARG:
+          if (".".equals(s)) {
+            result.isVarargs = true;
+            result.parameterTypes.add(forNameVararg(parameterType.toString()));
+            state = State.END;
+          } else {
+            parameterType.append(".");
+            parameterType.append(s);
+            state = State.IN_PARAM_TYPE;
+          }
+          break;
+        default:
+          break;
+      }
+    }
+    machine.pushToParameterStack(result);
+  }
+
+  @Override
+  public String getName() {
+    return ".<";
+  }
+
+  /**
+   * Tries to load the class from cache if exists.
+   *
+   * <p>If the type name contains '.', tries to look for the type name verbatim.
+   *
+   * <p>Otherwise looks in 'java.lang', 'java.util' and 'java.io' in order.
+   *
+   * <p>Throws if the type cannot be found.
+   *
+   * @throws MachineException
+   */
+  Class<?> forName(String typeName) {
+    if (typeName.contains(".")) {
+      try {
+        if (clazzCache.containsKey(typeName)) {
+          return clazzCache.get(typeName);
+        } else {
+          var clazz = Class.forName(typeName);
+          clazzCache.put(typeName, clazz);
+          return clazz;
+        }
+      } catch (ClassNotFoundException e) {
+        throw new MachineException(e.getMessage());
+      }
+    } else {
+      try {
+        var type = "java.lang.%s".formatted(typeName);
+        if (clazzCache.containsKey(type)) {
+          return clazzCache.get(type);
+        } else {
+          var clazz = Class.forName(type);
+          clazzCache.put(type, clazz);
+          return clazz;
+        }
+      } catch (ClassNotFoundException _e) {
+        try {
+          var type = "java.util.%s".formatted(typeName);
+          if (clazzCache.containsKey(type)) {
+            return clazzCache.get(type);
+          } else {
+            var clazz = Class.forName(type);
+            clazzCache.put(type, clazz);
+            return clazz;
+          }
+        } catch (ClassNotFoundException _ex) {
+          try {
+            var type = "java.io.%s".formatted(typeName);
+            if (clazzCache.containsKey(type)) {
+              return clazzCache.get(type);
+            } else {
+              var clazz = Class.forName(type);
+              clazzCache.put(type, clazz);
+              return clazz;
+            }
+          } catch (ClassNotFoundException e) {
+            throw new MachineException(e.getMessage());
+          }
+        }
+      }
+    }
+  }
+
+  Class<?> forNameVararg(String typeName) {
+    if (typeName.contains(".")) {
+      try {
+        var typeNameVararg = "[L%s;".formatted(typeName);
+        if (clazzCache.containsKey(typeNameVararg)) {
+          return clazzCache.get(typeNameVararg);
+        } else {
+          var clazz = Class.forName(typeNameVararg);
+          clazzCache.put(typeNameVararg, clazz);
+          return clazz;
+        }
+      } catch (ClassNotFoundException e) {
+        throw new MachineException(e.getMessage());
+      }
+    } else {
+      try {
+        var typeNameVararg = "[Ljava.lang.%s;".formatted(typeName);
+        if (clazzCache.containsKey(typeNameVararg)) {
+          return clazzCache.get(typeNameVararg);
+        } else {
+          var clazz = Class.forName(typeNameVararg);
+          clazzCache.put(typeNameVararg, clazz);
+          return clazz;
+        }
+      } catch (ClassNotFoundException _e) {
+        try {
+          var typeNameVararg = "[Ljava.util.%s;".formatted(typeName);
+          if (clazzCache.containsKey(typeNameVararg)) {
+            return clazzCache.get(typeNameVararg);
+          } else {
+            var clazz = Class.forName(typeNameVararg);
+            clazzCache.put(typeNameVararg, clazz);
+            return clazz;
+          }
+        } catch (ClassNotFoundException _ex) {
+          try {
+            var typeNameVararg = "[Ljava.io.%s;".formatted(typeName);
+            if (clazzCache.containsKey(typeNameVararg)) {
+              return clazzCache.get(typeNameVararg);
+            } else {
+              var clazz = Class.forName(typeNameVararg);
+              clazzCache.put(typeNameVararg, clazz);
+              return clazz;
+            }
+          } catch (ClassNotFoundException e) {
+            throw new MachineException(e.getMessage());
+          }
+        }
+      }
+    }
+  }
+}
---
+++ bjforth/src/test/java/bjforth/primitives/DOTLANGLETest.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2024 Bahman Movaqar
+ *
+ * This file is part of bjForth.
+ *
+ * bjForth is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * bjForth is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with bjForth. If not, see <https://www.gnu.org/licenses/>.
+ */
+package bjforth.primitives;
+
+import static bjforth.machine.BootstrapUtils.getPrimitiveAddress;
+import static bjforth.machine.MachineBuilder.aMachine;
+import static bjforth.machine.MachineStateBuilder.aMachineState;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import bjforth.primitives.DOTLANGLE.MethodDescriptor;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DOTLANGLETest {
+
+  private final InputStream originalSystemIn = System.in;
+
+  @AfterEach
+  public void restoreSystemIn() {
+    System.setIn(originalSystemIn);
+  }
+
+  @BeforeEach
+  public void resetKEY() {
+    PrimitiveFactoryModificationUtils.resetAllPrimitives();
+  }
+
+  @Test
+  void worksOkFullyQualified() {
+    // GIVEN
+    var str = "format(java.lang.String, java.lang.Object...)";
+    var inputStream = new ByteArrayInputStream(str.getBytes());
+    System.setIn(inputStream);
+
+    var DOTLANGLEaddr = getPrimitiveAddress(".<");
+    var actualState = aMachineState().withInstrcutionPointer(DOTLANGLEaddr).build();
+    var machine = aMachine().withState(actualState).build();
+
+    // WHEN
+    machine.step();
+
+    // THEN
+    var actualResult = (MethodDescriptor) machine.popFromParameterStack();
+    assertThat(actualResult.name).isEqualTo("format");
+    assertThat(actualResult.isVarargs).isTrue();
+    assertThat(actualResult.parameterTypes).isEqualTo(List.of(String.class, Object[].class));
+  }
+
+  @Test
+  void worksOkUnqualified() {
+    // GIVEN
+    var str = "format(List, File, String...)";
+    var inputStream = new ByteArrayInputStream(str.getBytes());
+    System.setIn(inputStream);
+
+    var DOTLANGLEaddr = getPrimitiveAddress(".<");
+    var actualState = aMachineState().withInstrcutionPointer(DOTLANGLEaddr).build();
+    var machine = aMachine().withState(actualState).build();
+
+    // WHEN
+    machine.step();
+
+    // THEN
+    var actualResult = (MethodDescriptor) machine.popFromParameterStack();
+    assertThat(actualResult.name).isEqualTo("format");
+    assertThat(actualResult.isVarargs).isTrue();
+    assertThat(actualResult.parameterTypes)
+        .isEqualTo(List.of(List.class, File.class, String[].class));
+  }
+}
